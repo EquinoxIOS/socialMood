3ebb3a995aecb1589045d132d000fad16158949a
Nephelim <nephelim@16h18.com>
2019-06-28 11:31:05 +0200

    Démarrage du projet vierge avec barre de navigation

---
+++ SocialMood/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SocialMood
+//
+//  Created by Néphélim Cohen on 28/06/2019.
+//  Copyright © 2019 Néphélim Cohen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SocialMood/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SocialMood
+//
+//  Created by Néphélim Cohen on 28/06/2019.
+//  Copyright © 2019 Néphélim Cohen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
